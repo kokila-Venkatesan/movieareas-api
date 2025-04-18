71a1e37bfc795466b1c09dc018b3d240a3cf0f02
Kokila-Venkatesan <kokilasmily@gmail.com>
2025-04-18 17:40:01 +0530

    routes proper formats with npm comments

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,24 +4,24 @@ const router = express.Router ()
 
 
 // for Reading movies
-router.get('/movies',() => {
-
+router.get('/',(req,res) => {
+    res.send ("Get all movies lists");
 })
 
 // for Creating movies
-router.post('/movies',() => {
-    
+router.post('/',(req, res) => {
+    res.send ("Create movies");
 })
 
 // for Updating movies
 
-router.put('/movies:id', () => {
-
+router.put('/:id', (req, res) => {
+    res.send ("Update movies");
 })
 
 // for Deleting movies 
-router.delete('/movies:id', ()=> {
-    
+router.delete('/:id', (req, res)=> {
+    res.send ("Delete movies");
 })
 
 export default router;
